f03f8ad60b2b2054c1f184d0a568518262b0a242
Jamie D. <45217971+jamie-dang-n@users.noreply.github.com>
2025-02-24 13:33:35 -0800

    initial commit

---
+++ spells_mods.py
@@ -0,0 +1,7 @@
+import json
+import zmq
+
+#CONSTANTS
+FIRST_LEVEL_PARAMS = ['index', 'name', 'level', 'url']
+SECOND_LEVEL_PARAMS = ['index', 'name', 'url', 'desc', 'higher_level', 'range', 'components', 'material', 'area_of_effect', 'ritual', 'duration', 'concentration', 'casting_time', 'level', 'attack_type', 'damage', 'school', 'classes', 'subclasses', 'url']
+
